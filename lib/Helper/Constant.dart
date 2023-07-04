f6e475fbfc2a9666d39b6416ca842907a013fd1a
AjayMalviya <ajay.malpahwizz@gmail.com>
2023-07-04 19:03:05 +0100

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,19 @@
+final String appName = 'Big Box';
+
+final String packageName = 'com.bigbox.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.bigbox.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.bigbox.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+final String baseUrl = 'https://bigboxx.co.in/app/v1/api/';
+final String imageUrl = 'https://bigboxx.co.in/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
